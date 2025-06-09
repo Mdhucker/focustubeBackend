9044f8151be6b02c9fd93c7ef1be7f0355b6c110
maulidimdami <maulidimdami@kali.home>
2025-06-09 21:07:06 +0300

    submodule

--- focustubeBase/settings.py
+++ focustubeBase/settings.py
@@ -2,10 +2,20 @@
 from decouple import config  # Import config from decouple this is used to load environment variables
 
 from pathlib import Path
-
 import os
-# from dotenv import load_dotenv
+import dj_database_url
 
+from environ import Env
+env = Env()
+Env.read_env()  # Reads the .env file if it exists
+ENVIRONMENT=env("ENVIRONMENT", default="production ")  # Get the environment variable, default to "development"
+
+
+
+
+
+
+# from dotenv import load_dotenv
 # load_dotenv()  # Load environment variables from .env
 
 
@@ -21,12 +31,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# SECRET_KEY = 'django-insecure-ix0fhowl+*n@nfmn=$bn_&cpq3vteo@_jh_bk*j#jtuk^fudgq'
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ix0fhowl+*n@nfmn=$bn_&cpq3vteo@_jh_bk*j#jtuk^fudgq'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if ENVIRONMENT == "development":
+    DEBUG = True
+else:
+    DEBUG = False
 # settings.py
 # REST_FRAMEWORK = {
 #     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
@@ -53,6 +67,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'api',
     'rest_framework_simplejwt',
+    # 'admin_honeypot',
 
 ]
 
@@ -122,12 +137,29 @@ WSGI_APPLICATION = 'focustubeBase.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
+# Default: use SQLite locally
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# If DATABASE_URL is set (like on Railway), use PostgreSQL
+POSTGRES_LOCALLY = True
+if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
+    DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
+
+
 
 
 # Password validation
@@ -170,3 +202,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# USERNAME_BLACKLIST = [
+#     'admin', 'root', 'support', 'staff', 'superuser',
+#     'moderator', 'help', 'null', 'none', 'me', 'system','abuu_luquman',
+# ]
--- focustubeBase/urls.py
+++ focustubeBase/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
